7ffe6a550dafeb1ced2a84c08b142f0dbefba61b
funiusa <funiusa@gmail.com>
2022-03-31 19:18:55 +0300

    change some smoll

--- stepik_simple_var.sh
+++ stepik_simple_var.sh
@@ -6,6 +6,17 @@ reset_color='\033[0m'
 
 path=$HOME
 
+__variable="__variable"
+variable124="va124"
+variable_134="v134"
+_variable="_v"
+VARiable="VAR"
+
+echo "Variable names:"
+echo "=========================================================================="
+echo $__variable" | "$variable124" | "$variable_134" | "$_variable" | "$VARiable
+echo "=========================================================================="
+
 file_name="Fi.op"
 
 echo "Hello world" > $file_name
